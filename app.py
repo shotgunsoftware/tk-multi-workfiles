e394f16477008099adfa8a5e460ce6b0464acb88
Tony Barbieri <tbarbieri@psyop.tv>
2013-05-06 17:42:23 -0400

    Adds read-only properties for querying the workfiles handler internal context.

--- app.py
+++ app.py
@@ -16,20 +16,24 @@ class MultiWorkFiles(tank.platform.Application):
         """
         Called as the application is being initialized
         """
-        
+
         tk_multi_workfiles = self.import_module("tk_multi_workfiles")
 
         # register commands:
         self._work_files_handler = tk_multi_workfiles.WorkFiles(self)
 
         self.engine.register_command("Tank File Manager...", self._work_files_handler.show_dlg)
-        
+
         cmd = lambda app=self: tk_multi_workfiles.SaveAs.show_save_as_dlg(app)
         self.engine.register_command("Tank Save As...", cmd)
-        
+
         cmd = lambda app=self: tk_multi_workfiles.Versioning.show_change_version_dlg(app)
         self.engine.register_command("Version up Current Scene...", cmd)
-        
+
+    @property
+    def workfiles_handler_context(self):
+        return self._work_files_handler.internal_context
+
     def destroy_app(self):
         self.log_debug("Destroying tk-multi-workfiles")
         self._work_files_handler = None
