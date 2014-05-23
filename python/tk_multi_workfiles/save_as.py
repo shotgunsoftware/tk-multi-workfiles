8689551d608fa5d27a423c2bef1c475fa81e18c9
Alan Dann <alan.dann@shotgunsoftware.com>
2014-05-23 09:35:08 +0100

    Fixes #25466 - folders are no longer created as part of Shotgun Save-As. Folder creation will still be run if the directory being saved to doesn't exist for some reason, for example if the Work Area has been changed without folders being created.

--- python/tk_multi_workfiles/save_as.py
+++ python/tk_multi_workfiles/save_as.py
@@ -169,14 +169,23 @@ class SaveAs(object):
         
     def save_as(self, new_path):
         """
-        Do actual save-as of the current scene as the new
-        path - assumes all validity checking has already
+        Do actual save-as of the current scene as the new path - assumes all validity checking has already
         been done
+        
+        :param new_path:    The new path to save the current script/scene to
         """
-        # always try to create folders:
-        ctx_entity = self._app.context.task or self._app.context.entity or self._app.context.project
-        if ctx_entity:
-            self._app.tank.create_filesystem_structure(ctx_entity.get("type"), ctx_entity.get("id"))
+        # we used to always create folders but this seems unnecessary as the folders should have been
+        # created when the work area was set - either as part of the launch process or when switching
+        # work area within the app.
+        # To be on the safe side though, we'll check if the directory that the file is being saved in
+        # to exists and run create folders if it doesn't - this covers any potential edge cases where
+        # the Work area has been set without folder creation being run correctly.
+        dir = os.path.dirname(new_path)
+        if not dir or not os.path.exists(dir):
+            ctx_entity = self._app.context.task or self._app.context.entity or self._app.context.project
+            if ctx_entity:
+                self._app.log_debug("Creating folders for context %s" % self._app.context)
+                self._app.tank.create_filesystem_structure(ctx_entity.get("type"), ctx_entity.get("id"))
         
         # and save the current file as the new path:
         save_file(self._app, SAVE_FILE_AS_ACTION, self._app.context, new_path)
