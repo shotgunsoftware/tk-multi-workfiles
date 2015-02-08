66fd8954027cf554e56bfb058b1dcdd3d76dd167
Lutz Paelike <lutz@delutze.de>
2015-02-08 20:07:12 +0100

    add modo hook

---
+++ hooks/scene_operation_tk-modo.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+
+import tank
+from tank import Hook
+from tank import TankError
+from tank.platform.qt import QtGui
+
+import shotgunsupport
+
+class SceneOperation(Hook):
+    """
+    Hook called to perform an operation with the 
+    current scene
+    """
+    
+    def execute(self, operation, file_path, context, parent_action, file_version, read_only, **kwargs):
+        """
+        Main hook entry point
+        
+        :param operation:       String
+                                Scene operation to perform
+        
+        :param file_path:       String
+                                File path to use if the operation
+                                requires it (e.g. open)
+                    
+        :param context:         Context
+                                The context the file operation is being
+                                performed in.
+                    
+        :param parent_action:   This is the action that this scene operation is
+                                being executed for.  This can be one of:
+                                - open_file
+                                - new_file
+                                - save_file_as 
+                                - version_up
+                        
+        :param file_version:    The version/revision of the file to be opened.  If this is 'None'
+                                then the latest version should be opened.
+        
+        :param read_only:       Specifies if the file should be opened read-only or not
+                            
+        :returns:               Depends on operation:
+                                'current_path' - Return the current scene
+                                                 file path as a String
+                                'reset'        - True if scene was reset to an empty 
+                                                 state, otherwise False
+                                all others     - None
+        """
+        
+        if operation == "current_path":
+            return shotgunsupport.get_scene_filename()
+        elif operation == "open":
+            shotgunsupport.load_file(file_path )
+        elif operation == "save":
+            shotgunsupport.save_scene()
+        elif operation == "save_as":
+            shotgunsupport.save_scene_as(file_path)
+        elif operation == "reset":
+            try:
+                shotgunsupport.reset_scene()
+                return  True
+            except:
+                return False
+
