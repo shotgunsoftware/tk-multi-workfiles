d69e5fc5b587dd14544e1df050e4cd68fd12bb59
Jeff Beeland <jeff.beeland@autodesk.com>
2015-10-16 17:58:12 -0700

    Looks up context specific app settings via engine instance name instead of engine system name.

--- python/tk_multi_workfiles/work_files.py
+++ python/tk_multi_workfiles/work_files.py
@@ -997,7 +997,12 @@ class WorkFiles(object):
         
         # find settings for all instances of app in 
         # the environment picked for the given context:
-        other_settings = tank.platform.find_app_settings(self._app.engine.name, self._app.name, self._app.tank, context)
+        other_settings = tank.platform.find_app_settings(
+            self._app.engine.instance_name,
+            self._app.name,
+            self._app.tank,
+            context,
+        )
         
         if len(other_settings) == 1:
             return other_settings[0].get("settings")
